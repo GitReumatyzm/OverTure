777c73aba97c794c503de611c785d7ede5994db4
Reumatyzm <jaroszewskakaja@gmail.com>
2023-04-12 02:51:40 +0200

    #4
    Countdown added

--- GameMakerGra2/Assets/Scripts/CarGrowing.cs
+++ GameMakerGra2/Assets/Scripts/CarGrowing.cs
@@ -5,42 +5,51 @@ using UnityEngine;
 public class CarGrowing : MonoBehaviour
 {
     public GameObject Player;
-
     public float maxScale = 2f;
     public float minScale = 0.5f;
 
-
-    void Start()
+    private void Start()
     {
-
-    Player = GameObject.FindGameObjectWithTag("Player");
-
+        Player = GameObject.FindGameObjectWithTag("Player");
     }
 
-    public void Update()
+    private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Alpha1))
-    {
-        Debug.Log("Rozmiar: " + Player.transform.localScale);
-        Player.transform.localScale *= 2f;
-        Player.transform.localScale = ClampScale(Player.transform.localScale);
-    }
-    else if (Input.GetKeyDown(KeyCode.Alpha2))
-    {
-         Debug.Log("Rozmiar: " + Player.transform.localScale);
-        Player.transform.localScale = ClampScale(Player.transform.localScale / 2f);
-        Debug.Log("Before clamp: " + Player.transform.localScale);
-        Player.transform.localScale = ClampScale(Player.transform.localScale);
-        Debug.Log("After clamp: " + Player.transform.localScale);
-    }
-
+        {
+            Debug.Log("Rozmiar: " + Player.transform.localScale);
+            Player.transform.localScale *= 1.5f;
+            Player.transform.localScale = ClampScale(Player.transform.localScale);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            Debug.Log("Rozmiar: " + Player.transform.localScale);
+            Player.transform.localScale /= 1.5f;
+            Player.transform.localScale = ClampScale(Player.transform.localScale);
+        }
     }
 
     private Vector3 ClampScale(Vector3 scale)
-     {
+{
     scale.x = Mathf.Clamp(scale.x, minScale, maxScale);
     scale.y = Mathf.Clamp(scale.y, minScale, maxScale);
     scale.z = Mathf.Clamp(scale.z, minScale, maxScale);
-    return scale;
+
+    if (scale.x < minScale)
+    {
+        scale.x = minScale;
     }
+
+    if (scale.y < minScale)
+    {
+        scale.y = minScale;
+    }
+
+    if (scale.z < minScale)
+    {
+        scale.z = minScale;
+    }
+
+    return scale;
+}
 }
---
+++ GameMakerGra2/Assets/Scripts/Countdown.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Countdown : MonoBehaviour 
+{
+    public float timeStart = 3;
+    public GameObject Player;
+    public TMP_Text textBox;
+    public Rigidbody playerRigidbody;
+
+
+    void Start() 
+    {
+        StartCoroutine(CountdownCoroutine());
+    }
+
+    IEnumerator CountdownCoroutine()
+    {
+        while (timeStart > 0)
+        {
+            playerRigidbody.constraints = RigidbodyConstraints.FreezeAll;
+            textBox.text = Mathf.Round(timeStart).ToString();
+            yield return new WaitForSeconds(1f);
+            timeStart--;
+        }
+        textBox.text = "JEĆ!";
+        yield return new WaitForSeconds(1f);
+        playerRigidbody.constraints = RigidbodyConstraints.None;
+        textBox.gameObject.SetActive(false);
+    }
+}
