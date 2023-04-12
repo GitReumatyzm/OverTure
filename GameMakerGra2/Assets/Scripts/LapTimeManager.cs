99d3f7d5d89414e7d1e2981db5fd55a1bdf51d3f
Reumatyzm <jaroszewskakaja@gmail.com>
2023-04-12 21:16:07 +0200

    #5
    - Highscore added

--- GameMakerGra2/Assets/Scripts/LapTimeManager.cs
+++ GameMakerGra2/Assets/Scripts/LapTimeManager.cs
@@ -7,40 +7,50 @@ using TMPro;
 public class LapTimeManager : MonoBehaviour
 {
     public float TimeStart;
-    public TMP_Text TextBox;
+
+    public TMP_Text LapTime;
+    public TMP_Text BestLapTime;
 
     public GameObject StartLap;
     public GameObject FinishLap;
+    public GameObject Player;
 
     bool IsLapStarted = false;
     
     void Start()
     {
-        TextBox.text = TimeStart.ToString("F2");
+        LapTime.text = TimeStart.ToString("F2");
+        BestLapTime.text = PlayerPrefs.GetFloat("BestLapTime", 0f).ToString("0.00");
+        PlayerPrefs.DeleteKey("BestLapTime");
     }
 
     void Update()
     {
         if(IsLapStarted == true)
         {
-          TimeStart += Time.deltaTime;
-          TextBox.text = TimeStart.ToString("F2");
+            TimeStart += Time.deltaTime;
+            LapTime.text = TimeStart.ToString("F2");
         }
     }
 
     public void OnTriggerEnter(Collider other)
-{
-    if (other.gameObject == FinishLap && other == FinishLap.GetComponent<Collider>())
     {
-        IsLapStarted = false; 
-        float totalTime = TimeStart; 
-        TimeStart = 0; 
-        TextBox.text = totalTime.ToString("F2"); 
+        if (other.gameObject == FinishLap && other == FinishLap.GetComponent<Collider>())
+        {
+            IsLapStarted = false; 
+            float totalTime = TimeStart; 
+            TimeStart = 0; 
+            LapTime.text = totalTime.ToString("F2"); 
+
+            if (totalTime < PlayerPrefs.GetFloat("BestLapTime", 0f) || PlayerPrefs.GetFloat("BestLapTime", 0f) == 0f)
+            {
+                PlayerPrefs.SetFloat("BestLapTime", totalTime);
+                BestLapTime.text = (PlayerPrefs.GetFloat("BestLapTime", 0f) == 0f) ? "0.00" : PlayerPrefs.GetFloat("BestLapTime", 0f).ToString("F2");
+            }
+        }
+        else if (other.gameObject == StartLap && other == StartLap.GetComponent<Collider>())
+        {
+            IsLapStarted = true;
+        }
     }
-    else if (other.gameObject == StartLap && other == StartLap.GetComponent<Collider>())
-    {
-        IsLapStarted = true;
-    }
-}
-    
 }
