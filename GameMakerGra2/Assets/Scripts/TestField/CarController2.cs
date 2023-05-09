ee2912b7da731bf9b8a9ff13145392c01497a54d
Reumatyzm <jaroszewskakaja@gmail.com>
2023-05-09 02:55:57 +0200

    #8.2
    working on raycast to new car controller

--- GameMakerGra2/Assets/Scripts/TestField/CarController2.cs
+++ GameMakerGra2/Assets/Scripts/TestField/CarController2.cs
@@ -4,20 +4,71 @@ using UnityEngine;
 
 public class CarController2 : MonoBehaviour
 {
-    private float moveInput;
-    public float forwardSpeed;
     public Rigidbody SphereRB;
+
+    private float SpeedInput;
+    private float TurnInput;
+
+    public float ForwardAccel;
+    public float ReverseAccel;
+    public float MaxSpeed;
+    public float TurnStrength;
+    public float GravityForce;
+    public float GroundRayLength;
+
+    private bool IsGrounded;
+
+    public Transform GroundRayPoint;
+
+    public LayerMask WhatIsGround;
+
+    void Start()
+    {
+        SphereRB.transform.parent = null;
+    }
 
     void Update()
     {
-       moveInput = Input.GetAxisRaw("Vertical");
-       moveInput *= forwardSpeed;
+        SpeedInput = 0f;
+        if(Input.GetAxis("Vertical") > 0)
+        {
+            SpeedInput = Input.GetAxis("Vertical") * ForwardAccel * 1000f;
+        }
+        else if(Input.GetAxis("Vertical") < 0)
+        {
+            SpeedInput = Input.GetAxis("Vertical") * ForwardAccel * 1000f;
+        }
 
-        transform.position =  SphereRB.transform.position;
+        TurnInput = Input.GetAxis("Horizontal");
+
+        if(IsGrounded)
+        {
+        transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles + new Vector3(0f, TurnInput * TurnStrength * Time.deltaTime * Input.GetAxis("Vertical"), 0f));
+        }
+
+        transform.position = SphereRB.transform.position;
     }
 
     void FixedUpdate()
     {
-        SphereRB.AddForce(transform.forward * moveInput, ForceMode.Acceleration);
+       IsGrounded = false;
+       RaycastHit Hit; 
+
+       if(Physics.Raycast(GroundRayPoint.position, transform.up, out Hit, GroundRayLength, WhatIsGround))
+       {
+          IsGrounded = true;
+       }
+
+        if(IsGrounded)
+        {
+               if(Mathf.Abs(SpeedInput) > 0)
+                {
+                   SphereRB.AddForce(transform.forward * SpeedInput);
+                }
+        }
+        else
+        {
+            SphereRB.AddForce(Vector3.up * -GravityForce * 100f);
+        }
     }
 }
