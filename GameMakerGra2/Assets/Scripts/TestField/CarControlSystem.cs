47d8055e7578ccabe6136672a552c3b2aeb2a001
Reumatyzm <jaroszewskakaja@gmail.com>
2023-05-14 02:22:14 +0200

    #8.5
    New movement is almost done, only the car sinking problem to fix

---
+++ GameMakerGra2/Assets/Scripts/TestField/CarControlSystem.cs
@@ -0,0 +1,88 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarControlSystem : MonoBehaviour
+{
+    public Rigidbody SphereRB;
+
+    private float SpeedInput;
+    private float TurnInput;
+
+    public float ForwardAccel = 8f;
+    public float ReverseAccel = 4f;
+    public float MaxSpeed = 50f;
+    public float TurnStrength = 180f;
+    public float GravityForce = 10f;
+    public float DragOnGround = 3f;
+    public float GroundRayLength = .5f;
+    public float MaxWheelTurn = 25f;
+
+    private bool IsGrounded;
+
+    public LayerMask WhatIsGround;
+
+    public Transform GroundRayPoint;
+    public Transform LeftFrontWheel;
+    public Transform RightFrontWheel;
+    
+
+    void Start()
+    {
+        SphereRB.transform.parent = null;
+    }
+
+    void Update()
+    {
+        SpeedInput = 0f;
+        if(Input.GetAxis("Vertical") > 0)
+        {
+            SpeedInput = Input.GetAxis("Vertical") * ForwardAccel * 1000f;
+        }
+        else if(Input.GetAxis("Vertical") < 0)
+        {
+            SpeedInput = Input.GetAxis("Vertical") * ForwardAccel * 1000f;
+        }
+
+        TurnInput = Input.GetAxis("Horizontal");
+
+        if(IsGrounded)
+        {
+        transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles + new Vector3(0f, TurnInput * TurnStrength * Time.deltaTime * Input.GetAxis("Vertical"), 0f));
+        }
+        
+        //LeftFrontWheel.localRotation = Quaternion.Euler(LeftFrontWheel.localRotation.eulerAngles.x, (TurnInput * MaxWheelTurn) - 180, LeftFrontWheel.localRotation.eulerAngles.z);
+        //RightFrontWheel.localRotation = Quaternion.Euler(RightFrontWheel.localRotation.eulerAngles.x, TurnInput * MaxWheelTurn, RightFrontWheel.localRotation.eulerAngles.z);
+
+        transform.position = SphereRB.transform.position;
+    }
+
+    void FixedUpdate()
+    {
+        IsGrounded = false;
+        RaycastHit hit; 
+        
+        if(Physics.Raycast(GroundRayPoint.position, -transform.up, out hit, GroundRayLength, WhatIsGround))
+        {
+            IsGrounded = true;
+
+            transform.rotation = Quaternion.FromToRotation(transform.up, hit.normal) * transform.rotation;
+        }
+
+        if(IsGrounded)
+        {
+            SphereRB.drag = DragOnGround;
+
+               if(Mathf.Abs(SpeedInput) > 0)
+                {
+                   SphereRB.AddForce(transform.forward * SpeedInput);
+                }
+        }
+        else
+        {
+            SphereRB.drag = 0.1f;
+            SphereRB.AddForce(Vector3.up * -GravityForce * 100f);
+        }
+               
+    }
+}
