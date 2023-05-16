326d36af11bb64f325d35121c1a38334ce394acf
GitReumatyzm <jaroszewskakaja@gmail.com>
2023-05-16 11:26:26 +0200

    #11.4
    MENU MINOR CHANGES

--- GameMakerGra2/Assets/Scripts/Menu/MainMenu.cs
+++ GameMakerGra2/Assets/Scripts/Menu/MainMenu.cs
@@ -7,7 +7,7 @@ public class MainMenu : MonoBehaviour
 {
     public void PlayGame()
     {
-        SceneManager.LoadScene(0);
+        SceneManager.LoadScene(2);
     }
     
     public void QuitGame()
